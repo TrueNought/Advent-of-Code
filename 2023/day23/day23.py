1b77e67e06547744e7d6b9a180181cf6ff493fd6
Roland Zhao <roland.zhao@mail.utoronto.ca>
2023-12-30 18:36:16 -0500

    day23 solution

--- 2023/day23/day23.py
+++ 2023/day23/day23.py
@@ -1,14 +1,18 @@
+"""
+Credits to HyperNeutrino for the edge contraction approach used to simplify the number of total nodes that need to be considered when finding the longest path.
+"""
+
 with open('day23_input.txt') as input:
   input = input.read().splitlines()
 
 slopes = {'^': (-1, 0), 'v': (1, 0), '<': (0, -1), '>': (0, 1)}
 
 def part_one(grid):
-  longest = 0
-  start = (0, next(j for j in range(len(grid[0])) if grid[0][j] == '.'))
-  goal = (len(grid)-1, next(j for j in range(len(grid[-1])) if grid[-1][j] == '.'))
+  start = (0, grid[0].index('.'))
+  goal = (len(grid)-1, grid[-1].index('.'))
   
   s = [(start, 0, set())]
+  longest = 0
 
   while s:
     (curr, steps, visited) = s.pop()
@@ -35,29 +39,58 @@ def part_one(grid):
   return longest
 
 def part_two(grid):
-  longest = 0
-  start = (0, next(j for j in range(len(grid[0])) if grid[0][j] == '.'))
-  goal = (len(grid)-1, next(j for j in range(len(grid[-1])) if grid[-1][j] == '.'))
+  start = (0, grid[0].index('.'))
+  goal = (len(grid)-1, grid[-1].index('.'))
+
+  points = [start, goal]
   
-  s = [(start, 0, set())]
+  for r, row in enumerate(grid):
+    for c, ch in enumerate(row):
+      if ch == '#':
+        continue
 
-  while s:
-    (curr, steps, visited) = s.pop()
-    print(curr)
-    
+      neighbors = 0
+      for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+        if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] != '#':
+          neighbors += 1
+
+      if neighbors >= 3:
+        points.append((r, c))
+
+  graph = {p: {} for p in points}
+  
+  for sr, sc in points:
+    stack = [(sr, sc, 0)]
+    visited = {(sr, sc)}
+
+    while stack:
+      r, c, n = stack.pop()
+
+      if n != 0 and (r, c) in points:
+        graph[(sr, sc)][(r, c)] = n
+        continue
+
+      for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+        if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] != '#' and (nr, nc) not in visited:
+          stack.append((nr, nc, n + 1))
+          visited.add((nr, nc))
+
+  visited = set()
+  def dfs(curr):
     if curr == goal:
-      longest = max(longest, steps)
-      print('got to goal with', longest)
-      continue
-
+      return 0
+    
+    length = float('-inf')
     visited.add(curr)
-    r, c = curr
 
-    for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
-      if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and (nr, nc) not in visited and grid[nr][nc] != '#':
-          s.append(((nr, nc), steps + 1, set(visited)))        
+    for neighbor in graph[curr]:
+      if neighbor not in visited:
+        length = max(length, dfs(neighbor) + graph[curr][neighbor])
+    visited.remove(curr)
 
-  return longest
+    return length
+  
+  return dfs(start)
 
-# print('part1', part_one(input))
+print('part1', part_one(input))
 print('part2', part_two(input))
